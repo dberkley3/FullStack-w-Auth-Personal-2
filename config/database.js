392d528b5d983a304b90f5fe97104a091aafe67d
dberkley3 <dberkley3@gmail.com>
2022-05-31 04:44:28 -0400

    Full Stack Auth Jokes initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mmongodb+srv://jokes:deebee@cluster0.mnem8.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'jokes'
+};
